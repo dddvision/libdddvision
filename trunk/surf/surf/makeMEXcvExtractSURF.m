60526bffc0d413e3e4fa2ada8a1d992f33575eeb
dddvision <dddvision@5c6639e4-15fb-11df-801b-2d21f7a92e13>
2010-08-10 18:53:54 +0000

    adding updated surf demo code

---
+++ trunk/surf/surf/makeMEXcvExtractSURF.m
@@ -0,0 +1,30 @@
+fprintf('\nMaking MEXcvExtractSurf.cpp....');
+userPath = path;    
+userWarnState=warning('off','all'); % see MATLAB Solution ID 1-5JUPSQ
+addpath(getenv('LD_LIBRARY_PATH'),'-END');
+addpath(getenv('PATH'),'-END');
+warning(userWarnState);
+if(ispc)
+  libdir=fileparts(which('cv110.lib'));
+elseif(ismac)
+  libdir=fileparts(which('libcv.dylib'));
+else
+  libdir=fileparts(which('libcv.so'));
+end
+path(userPath);
+userDirectory = pwd;
+try
+  if(ispc)    
+    mex('MEXcvExtractSURF.cpp',['-L"',libdir,'"'],'-lcv110','-lcxcore110', '-lhighgui110', '-lcvaux110');
+  elseif(ismac)
+    mex('MEXcvExtractSURF.cpp',['-L"',libdir,'"'],'-lcv','-lcxcore', '-lhighgui', '-lcvaux');
+  else
+    mex('MEXcvExtractSURF.cpp',['-L"',libdir,'"'],'-lcv','-lcxcore', '-lhighgui', '-lcvaux');
+  end
+catch err
+   details =['mex fail' err.message];
+   cd(userDirectory);
+   error(details);
+end
+cd(userDirectory);     
+fprintf('Done\n');
