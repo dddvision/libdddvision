f671cc9cfce35b345a64af31078a3b53eddebeee
dddvision <dddvision@5c6639e4-15fb-11df-801b-2d21f7a92e13>
2010-12-28 19:55:12 +0000

    Superficial updates to ReadKeys and invJexp

--- ReadKeys.m
+++ ReadKeys.m
@@ -1,77 +1,76 @@
-function [T,X,Q,Qd,n] = ReadKeys(filename)
-% Loads animation keyframe motion data.
-% The input system of units is seconds, meters, and degrees.
+% Loads motion data from a file
 %
-% COORDINATE SYSTEM:
-% Translation, relative to earth fixed frame
-%   X = North 
-%   Y = East
-%   Z = Geodetic Down
+% INPUT
+% filename = name of the file containing key poses
 %
-% Euler orientation
-%   A = pan right
-%   B = tilt up
-%   C = spin clockwise
-%
-% Euler rates, expressed in body axes
-%   D = pan right about rotated A axis
-%   E = tilt up about rotated B axis
-%   F = spin clockwise about rotated C axis
-%
-% FILE FORMAT:
-% [time stamp] [X] [Y] [Z] [A] [B] [C] [D] [E] [F]
-%    . . .
-%    . . .
-%
-% RETURN VALUES:
-% Aerospace style (right-hand, X-forward, Z-down, radians)
-%  T = time stamp vector (1-by-n)
-%  X = camera positions in world frame (3-by-n)
-%  Q = quaternions in unrotated frame (scalar-vector) (4-by-n)
-% Qd = quaternion rates in unrotated frame (4-by-n)
+% OUTPUT
+%  T = time stamp vector in seconds (1-by-n)
+%  X = body positions in world frame in meters (3-by-n)
+%  Q = quaternion orientation of Forward-Right-Down frame relative to world frame (4-by-n)
+% Qd = quaternion rates (4-by-n)
 %  n = number of keys read
+%
+% NOTES
+% The input file is tab separated in the following format
+%
+% T  X  Y  Z  A  B  C  D  E  F
+%    . . .
+%    . . .
+%
+% T = time stamp in seconds
+% X = North in meters
+% Y = East in meters
+% Z = Geodetic Down in meters
+% A = Euler pan right in degrees
+% B = Euler tilt up in degrees
+% C = Euler spin clockwise in degrees
+% D = pan right about rotated A axis in degrees per second
+% E = tilt up about rotated B axis in degrees per second
+% F = spin clockwise about rotated C axis in degrees per second
+function [T, X, Q, Qd, n] = ReadKeys(filename)
 
-%read keyframe data
-K=dlmread(filename,'\t');
+  % read keyframe data
+  K = dlmread(filename);
 
-K=K'; %change formats
-[m,n]=size(K);
+  K = K'; % transpose
+  [m, n] = size(K);
 
-if m~=10
-    error([filename,' must contain m-by-10 tab delimited matrix']);
+  if(m~=10)
+    error([filename, ' must contain m-by-10 tab delimited matrix']);
+  end
+
+  if(sum(K(:, end)==0)==10)
+    fprintf('\nwarning: last line of keyframe file contains zeros or whitespace'); 
+  end
+
+  % extract time stamp
+  T = K(1, :);
+  if(sum(double(diff(T)<0.000001))>0)
+    error('keyframe times must increase monotonically and significantly');
+  end
+
+  % extract position
+  X = K(2:4, :);
+
+  % convert to radians
+  K(5:10, :) = K(5:10, :)*(pi/180);
+
+  % extract Euler angles
+  pan = K(5, :);
+  tilt = K(6, :);
+  spin = K(7, :);
+
+  % extract Euler rates (reverse order)
+  er(3, :) = K(8, :);
+  er(2, :) = K(9, :);
+  er(1, :) = K(10, :);
+
+  % convert from pan-tilt-spin (reverse order) to scalar-vector quaternions
+  Q = Euler2Quat([spin; tilt; pan]);
+
+  Qd = zeros(4, n);
+  for i = 1:n
+    Qd(:, i) = 0.5*Quat2Homo(Q(:, i))*[0; er(:, 1)]; % qd = 1/2*q*omega
+  end
+
 end
-if sum(K(:,end)==0)==10
-	warning('last line of keyframe file contains zeros or whitespace'); 
-end
-
-%extract time stamp
-T=K(1,:);
-if sum(double(diff(T)<0.000001))>0
-	error('keyframe times must increase monotonically and significantly');
-end
-
-%extract position
-X=K(2:4,:);
-
-%convert to radians
-K(5:10,:)=K(5:10,:)*(pi/180);
-
-%extract Euler angles
-pan=K(5,:);
-tilt=K(6,:);
-spin=K(7,:);
-
-%extract Euler rates (reverse order)
-er(3,:)=K(8,:);
-er(2,:)=K(9,:);
-er(1,:)=K(10,:);
-
-%convert from pan-tilt-spin (reverse order) to scalar-vector quaternions
-Q=Euler2Quat([spin;tilt;pan]);
-
-Qd=zeros(4,n);
-for i=1:n
-  Qd(:,i)=0.5*Quat2Homo(Q(:,i))*[0;er(:,1)]; %qd=1/2*q*omega
-end
-
-return
--- invJexp.m
+++ invJexp.m
@@ -1,32 +1,28 @@
-function invJ=invJexp(q,opt)
-% Jacobian mapping of quaternion rate to axis-angle or axis-halfangle rate.
+% Jacobian mapping of quaternion rate to axis-angle rate
 %
 % q = orientation in quaternion form (4-by-1)
-%
 % J = Jacobian (3-by-4)
-% opt = interpret output argument as (1) axis-angle or (2) axis-halfangle
+% opt = (obsolete) interpret output argument as (1) axis-angle or (2) axis-halfangle
+function J = invJexp(q, opt)
+  q1 = q(1);
+  q2 = q(2);
+  q3 = q(3);
+  q4 = q(4);
 
-q1=q(1);
-q2=q(2);
-q3=q(3);
-q4=q(4);
+  s = sqrt(1-q1^2);
+  qvm = sqrt(q2^2+q3^2+q4^2);
+  qvm3 = qvm^3;
+  ac = acos(q1);
 
-s=sqrt(1-q1^2);
-qvm=sqrt(q2^2+q3^2+q4^2);
-qvm3=qvm^3;
-ac=acos(q1);
+  qvm(qvm<eps) = eps;
+  qvm3(qvm3<eps) = eps;
+  s(s<eps) = eps;
 
-ep=1E-12;
-qvm(qvm<ep)=ep;
-qvm3(qvm3<ep)=ep;
-s(s<ep)=ep;
+  J = [[-1/s*q2/qvm, ac/qvm-ac*q2^2/qvm3, -ac*q2/qvm3*q3     , -ac*q2/qvm3*q4     ]
+       [-1/s*q3/qvm, -ac*q2/qvm3*q3     , ac/qvm-ac*q3^2/qvm3, -ac*q3/qvm3*q4     ]
+       [-1/s*q4/qvm, -ac*q2/qvm3*q4     , -ac*q3/qvm3*q4     , ac/qvm-ac*q4^2/qvm3]];
 
-invJ=[[-1/s*q2/qvm, ac/qvm-ac*q2^2/qvm3, -ac*q2/qvm3*q3     , -ac*q2/qvm3*q4     ]
-      [-1/s*q3/qvm, -ac*q2/qvm3*q3     , ac/qvm-ac*q3^2/qvm3, -ac*q3/qvm3*q4     ]
-      [-1/s*q4/qvm, -ac*q2/qvm3*q4     , -ac*q3/qvm3*q4     , ac/qvm-ac*q4^2/qvm3]];
-
-if opt==1
-  invJ=invJ*2;
+  if(opt==1)
+    J=J*2;
+  end
 end
-
-return
