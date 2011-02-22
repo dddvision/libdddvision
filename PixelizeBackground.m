598eeddf53a3171f81a9073001a5e5ad361e6a91
dddvision <dddvision@5c6639e4-15fb-11df-801b-2d21f7a92e13>
2011-02-22 22:36:52 +0000

    Adding PixelizeBackground and updating a few minor functions.

---
+++ PixelizeBackground.m
@@ -0,0 +1,53 @@
+function RGB = PixelizeBackground(rgb, mask)
+
+  numLevels = 4;
+
+  r = PBPrepareImage(rgb(:, :, 1), numLevels);
+  g = PBPrepareImage(rgb(:, :, 2), numLevels);
+  b = PBPrepareImage(rgb(:, :, 3), numLevels);
+  
+  R = r;
+  G = g;
+  B = b;
+  
+  bg = PBPrepareImage(mask, numLevels);
+  bg = double(bg>0.5);
+  bg = Smooth2(bg, [3, 3], 1);
+  fg = 1-bg;
+
+  for level = 2:numLevels
+    r = Reduce2(r);
+    g = Reduce2(g);
+    b = Reduce2(b);
+  end
+
+  for level = 2:numLevels
+    r = Expand2(r);
+    g = Expand2(g);
+    b = Expand2(b);
+  end
+  
+  R = bg.*r+fg.*R;
+  G = bg.*g+fg.*G;
+  B = bg.*b+fg.*B;
+  
+  RGB = cat(3, R, G, B);
+
+end
+
+function img = PBPrepareImage(img, numLevels)
+  img = double(img)/255;
+  multiple = 2^(numLevels-1);
+  [M, N] = size(img);
+  Mpad = multiple-mod(M, multiple);
+  Npad = multiple-mod(N, multiple);
+  if((Mpad>0)||(Npad>0))
+    img(M+Mpad, N+Npad) = 0; % allocates memory for padded image
+  end
+  if(Mpad>0)
+    img((M+1):(M+Mpad), :) = NaN;
+  end  
+  if(Npad>0)
+    img(:, (N+1):(N+Npad)) = NaN;
+  end
+end
--- Reduce2.m
+++ Reduce2.m
@@ -1,7 +1,13 @@
-% Recuce2() computes smaller layer of a conservative pyramid
-function smallIm=Reduce2(im)
-  mask = [1,1;1,1];
-  Result = conv2(im, mask);
-  smallIm = Result(1:2:(end-1),1:2:(end-1));
+% Computes smaller layer of a conservative pyramid
+function y = Reduce2(x)
+  [M, N] = size(x);
+  if(mod(M, 2))
+    x(M+1, :) = x(M, :);
+    M = M+1;
+  end
+  if(mod(N, 2))
+    x(:, N+1) = x(:, N);
+    N = N+1;
+  end
+  y = x(1:2:M, 1:2:N)+x(2:2:M, 1:2:N)+x(1:2:M, 2:2:N)+x(2:2:M, 2:2:N);
 end
-
