a5eb18569d9992b6a995e33088ae9b2566689dd6
dddvision@gmail.com <dddvision@gmail.com@5c6639e4-15fb-11df-801b-2d21f7a92e13>
2011-05-17 20:02:16 +0000

    Merged bibliography changes from PLANS 2010. Updated GravityModel and Quat2AxisAngle documentation.

--- Quat2AxisAngle.m
+++ Quat2AxisAngle.m
@@ -1,28 +1,26 @@
 function v=Quat2AxisAngle(q)
-q=QuatNorm(q);
+  q = QuatNorm(q);
 
-q1=q(1,:);
-q2=q(2,:);
-q3=q(3,:);
-q4=q(4,:);
+  q1 = q(1, :);
+  q2 = q(2, :);
+  q3 = q(3, :);
+  q4 = q(4, :);
 
-theta=2*acos(q1);
-n=sqrt(q2.*q2+q3.*q3+q4.*q4);
+  theta = 2*acos(q1);
+  n = sqrt(q2.*q2+q3.*q3+q4.*q4);
 
-if isnumeric(q)
-  ep=1E-12;
-  n(n<ep)=ep;
+  if(isnumeric(q))
+    ep = 1E-12;
+    n(n<ep) = ep;
+  end
+
+  a = q2./n;
+  b = q3./n;
+  c = q4./n;
+
+  v1 = theta.*a;
+  v2 = theta.*b;
+  v3 = theta.*c;
+
+  v = [v1; v2; v3];
 end
-
-a=q2./n;
-b=q3./n;
-c=q4./n;
-
-v1=theta.*a;
-v2=theta.*b;
-v3=theta.*c;
-
-v=[v1;v2;v3];
-
-end
-
