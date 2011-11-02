8cfb00a7cf9ab5614d39283df2933e2ab23c74a5
dddvision <dddvision@5c6639e4-15fb-11df-801b-2d21f7a92e13>
2011-11-02 13:11:20 +0000

    Adding functionality

---
+++ ImageMerge.m
@@ -0,0 +1,19 @@
+function C = ImageMerge(A, B)
+  [AL, AH] = IMdecomp(A);
+  [BL, BH] = IMdecomp(B);
+  C = (AL+BH)/2;
+end
+
+function [L, H] = IMdecomp(A)
+  A1 = A;
+  A2 = Reduce(A1);
+  A3 = Reduce(A2);
+  A4 = Reduce(A3);
+  A5 = Reduce(A4);
+  A4 = Expand(A5);
+  A3 = Expand(A4);
+  A2 = Expand(A3);
+  A1 = Expand(A2);
+  L = A1;
+  H = A-L+mean(L(:));
+end
