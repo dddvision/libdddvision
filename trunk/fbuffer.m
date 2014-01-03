e182c01a3f2ba8cd7f2a0df2b3a846e523e24b3d
dddvision <dddvision@5c6639e4-15fb-11df-801b-2d21f7a92e13>
2014-01-03 22:31:55 +0000

    Updates to AffineAlignment and fbuffer

--- trunk/fbuffer.m
+++ trunk/fbuffer.m
@@ -1,28 +1,39 @@
-% Captures a figure via an offscreen buffer
+% Captures a figure using hardcopy.
 % 
-% INPUT
-% hfig = handle to a matlab figure
-% 
-% OUTPUT
-% cdata = color image in uint8, M-by-N-by-3
-%
-% TODO: update pos(1:2) if figure/axis units are normalized
-function cdata = fbuffer(hfig)
-  pos = get(hfig, 'Position');
-  if(strcmp(get(hfig, 'Renderer'), 'opengl'))
-    sppi = get(0, 'ScreenPixelsPerInch');
-    ppos = get(hfig, 'PaperPosition');
-    pos(1:2) = 0;
-    set(hfig, 'PaperPosition', pos./sppi);
-    cdata = hardcopy(hfig, '-dopengl', ['-r', num2str(round(sppi))]);
-    set(hfig, 'PaperPosition', ppos);
+% @param[in]  hFigure figure handle
+% @param[out] cData   truecolor image data
+function cData = fbuffer(hFigure)
+  units = get(hFigure, 'Units');
+  set(hFigure, 'Units', 'pixels');
+  position = get(hFigure, 'Position');
+
+  paperPositionMode = get(hFigure, 'PaperPositionMode');
+  set(hFigure, 'PaperPositionMode', 'Manual');
+  
+  paperUnits = get(hFigure, 'PaperUnits');
+  set(hFigure, 'PaperUnits', 'inches');
+  paperPosition = get(hFigure, 'PaperPosition');
+  
+  invertHardcopy = get(hFigure, 'InvertHardcopy');
+  set(hFigure, 'InvertHardcopy', 'off');
+  
+  if(strcmp(get(hFigure, 'Renderer'), 'opengl'))
+    sPPI = get(0, 'ScreenPixelsPerInch');
+    set(hFigure, 'PaperPosition', [0.0, 0.0, position(3)/sPPI, position(4)/sPPI]); 
+    cData = hardcopy(hFigure, '-dopengl', '-r0');
   else
-    mode = get(hfig, 'PaperPositionMode');
-    set(hfig, 'PaperPositionMode', 'auto', 'InvertHardcopy', 'off');
-    cdata = hardcopy(hfig, '-dzbuffer', '-r0');
-    set(hfig, 'PaperPositionMode', mode);
+    sPPI = get(0, 'ScreenPixelsPerInch');
+    set(hFigure, 'PaperPosition', [0.0, 0.0, position(3)/sPPI, position(4)/sPPI]); 
+    cData = hardcopy(hFigure, '-dzbuffer', '-r0');
   end
-  if(numel(cdata)>(pos(3)*pos(4)*3))
-    cdata = cdata(1:pos(4), 1:pos(3), :);
+  if(numel(cData)>(position(3)*position(4)*3))
+    cData = cData(1:position(4), 1:position(3), :);
   end
+
+  set(hFigure, 'InvertHardcopy', invertHardcopy);
+  set(hFigure, 'PaperUnits', paperUnits);
+  set(hFigure, 'PaperPosition', paperPosition);
+  set(hFigure, 'PaperPositionMode', paperPositionMode);
+  set(hFigure, 'Units', units);
+  set(hFigure, 'Position', position);
 end
