0a9c8f64a73f06cb4c22ce9590655f5610ea18fb
dddvision <dddvision@5c6639e4-15fb-11df-801b-2d21f7a92e13>
2014-11-18 12:59:05 +0000

    Fixed scaling problem in fineEdge.m

--- fineEdge.m
+++ fineEdge.m
@@ -1,19 +1,19 @@
-% Scales the input image dimensions by 2 and returns arbitrarily scaled gradients.
+% Scales the input image dimensions by 2 and returns smoothed gradients.
 %
 % @param[in]  x  real valued image in the range [0, 1]
 % @param[out] gi gradient in the down direction
 % @param[out] gj gradient in the right direction
 %
 % @note
-% Smoothing works in conjunction with lanczos scaling to estimate gradients at a finer scale.
+% Smoothing works in conjunction with a scaling kenel to estimate gradients at a finer scale.
 % The image border is extended in order to pre-adjust for the loss of border in the gradient operation.
-% Several operations affect the scale of the gradient magnitude in ways that are difficult to predict.
 function [gi, gj] = fineEdge(x)
 if(nargin==0)
   close('all');
   clear('classes');  
-  x = imread('saturn.png');
+  x = imread('peppers.png');
   x = double(rgb2gray(x))/255.0;
+  %x = [0, 0, 0; 0, 1, 0; 0, 0, 0];
   figure,imshow(x);
   [gi, gj] = fineEdge(x);
   gm = sqrt(gi.*gi+gj.*gj);
@@ -23,13 +23,14 @@ if(nargin==0)
   gj = [];
   return;
 end
-[M, N] = size(x);
 x = addBorder(x);
 x = smoothWindow(x, [1, 3], 0.5);
 x = smoothWindow(x, [3, 1], 0.5);
-x = imresize(x, [2*M+1, 2*N+1], 'lanczos3');
-x = smoothWindow(x, [1, 3], 0.5);
-x = smoothWindow(x, [3, 1], 0.5);
+x = imresize(x, 2.0, 'lanczos3');
+x = shrinkByOne(x);
+x = removeBorder(x);
+%x = smoothWindow(x, [1, 3], 0.5);
+%x = smoothWindow(x, [3, 1], 0.5);
 [gd, gc] = robertsCross(x);
 [r, t] = cartesianToPolar(gd, gc);
 t = t+pi/4.0; % rotate angle
@@ -38,6 +39,19 @@ end
 
 function x = addBorder(x)
 x = [x(1, 1), x(1, :), x(1, end); x(:, 1), x, x(:, end); x(end, 1), x(end, :), x(end, end)];
+end
+
+function x = removeBorder(x)
+x = x(2:(end-1), 2:(end-1));
+end
+
+function x = shrinkByOne(x)
+[M, N] = size(x);
+ma = 1:(M-1);
+mb = 2:M;
+na = 1:(N-1);
+nb = 2:N;
+x = (x(ma, na)+x(ma, nb)+x(mb, na)+x(mb, nb))/4.0;
 end
 
 function x = smoothWindow(x, win, sig)
