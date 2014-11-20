bdf500e456ca3b88c8f6f160b93a06ee271cb1bb
dddvision <dddvision@5c6639e4-15fb-11df-801b-2d21f7a92e13>
2014-11-20 20:11:39 +0000

    Improved algorithm for fineEdge and added maxima

--- trunk/Expand2.m
+++ trunk/Expand2.m
@@ -1,10 +1,21 @@
-% Computes smaller layer of a conservative pyramid
+% Computes smaller layer of a conservative pyramid.
 function y = Expand2(x)
-  [M, N] = size(x);
+[M, N, P] = size(x);
+if(P==1)
   y = zeros(2*M, 2*N);
   x = x/4;
   y(1:2:end, 1:2:end) = x;
   y(2:2:end, 1:2:end) = x;
   y(1:2:end, 2:2:end) = x;
   y(2:2:end, 2:2:end) = x;
+else
+  y = zeros(2*M, 2*N, P);
+  x = x/4;
+  for p = 1:P
+    y(1:2:end, 1:2:end, p) = x(:, :, p);
+    y(2:2:end, 1:2:end, p) = x(:, :, p);
+    y(1:2:end, 2:2:end, p) = x(:, :, p);
+    y(2:2:end, 2:2:end, p) = x(:, :, p);
+  end
+end
 end
