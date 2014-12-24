42f103de7f5c62c219c58c5a2558fe2dd83258a0
dddvision <dddvision@5c6639e4-15fb-11df-801b-2d21f7a92e13>
2014-12-24 00:38:12 +0000

    Added test case to SparseProjectiveAlignment

--- trunk/SparseProjectiveAlignment.m
+++ trunk/SparseProjectiveAlignment.m
@@ -15,7 +15,7 @@
 %    [a3 a4 b2]
 %    [c1 c2 1 ]]
 % lowPeaks = corners found in the new image
-% xyCov = estimate of the covariance of the coordinate transformation on a per pixel basis 
+% xyCov = estimate of the covariance of the coordinate transformation on a per pixel basis
 %
 % NOTES
 % The coordinate system origin is at the image center
@@ -44,11 +44,11 @@
 % [Mann1997] Steve Mann and Rosalind W. Picard, "Video Orbits of the Projective Group: A Simple Approach to Featureless
 % Estimation of Parameters," IEEE Trans. on Image Processing, v.6, 1997.
 %
-% A = 
+% A =
 % [ x, y, 1, 0, 0, 0, -x*xs, -xs*y]
 % [ 0, 0, 0, x, y, 1, -x*ys, -y*ys]
 %
-% X = 
+% X =
 % [a1]
 % [a2]
 % [b1]
@@ -81,243 +81,365 @@
 % [                ys]
 % [-x*xs*xs - x*ys*ys]
 % [-y*xs*xs - y*ys*ys]
-%
-% EXAMPLE
-% method = 'HarrisCorner';
-% indices = 73:136;
-% halfwin = 2;
-% sigma = 2;
-% numPoints = 300;
-% numIterations = 3;
-% stdThresh = 0.005;
-% border = 20;
-% levels = 2;
-% yold = getImage(1);
-% [gi, gj] = ComputeDerivatives2(yold);
-% kappa = DetectCorners(gi, gj, halfwin, method);
-% allPeaks = (kappa==LocalMAX(kappa,[3,3]));
-% allPeaks = allPeaks&(LocalSTD(yold, [1, 1]+2*halfwin)>stdThresh)&RemoveBorders(ones(size(yold)), border);
-% highPeaks = find(allPeaks);
-% y = getImage(2);
-% mask = (LocalSTD(y, [1, 1]+2*halfwin)>stdThresh)&RemoveBorders(ones(size(y)), border);
-% [P, lowPeaks, xyCov1] = SparseProjectiveAlignment(y, highPeaks, method, sigma, numPoints, numIterations, mask);
-% [P, xyCov2] = ProjectiveAlignment(y, yold, levels, numIterations, P, mask);
-  
+
 function [P,lowPeaks,xyCov]=SparseProjectiveAlignment(newImage,highPeaks,method,sigma,numPoints,numIterations,mask)
 
-  % initialize P
-  P=eye(3);
+if(nargin==0)
+  testScale = 1.1;
+  method = 'HarrisCorner';
+  halfwin = 2;
+  sigma = 2;
+  numPoints = 100;
+  numIterations = 100;
+  border = 20;
+  yold = double(imread('cameraman.tif'))/255.0;
+  y = imresize(yold, testScale);
+  y = y(1:size(yold, 1), 1:size(yold, 2));
+  mask = RemoveBorders(ones(size(yold)), border);
+  [gi, gj] = ComputeDerivatives2(yold);
+  kappa = DetectCorners(gi, gj, halfwin, method);
+  allPeaks = (kappa==LocalMAX(kappa,[3, 3]));
+  allPeaks = allPeaks&mask;
+  highPeaks = find(allPeaks);
+  [~, ind] = sort(kappa(highPeaks), 'descend');
+  highPeaks = highPeaks(ind(1:numPoints));
+  tic; [P, lowPeaks, xyCov] = SparseProjectiveAlignment(y, highPeaks, method, sigma, numPoints, numIterations, mask); toc;
+  P
+  xyCov
+  return;
+end
 
-  % get image size
-  [M,N]=size(newImage);
+% initialize P
+P=eye(3);
+
+% get image size
+[M,N]=size(newImage);
+
+% compute gradients
+[newGradx,newGrady]=ComputeDerivatives2(newImage);
+
+% compute corner intensity
+kappa=DetectCorners(newGradx,newGrady,sigma,method);
+
+% find peaks
+allPeaks=(kappa==LocalMAX(kappa,[3,3]));
+
+% only select masked region
+if(nargin>6)
+  allPeaks=allPeaks&mask;
+end
+
+% find linear indices of low peaks
+peakIndices=find(allPeaks);
+[val,ind]=sort(kappa(peakIndices),'descend');
+lowPeaks=peakIndices(ind(1:min(numPoints,numel(ind))));
+
+if(~isempty(highPeaks))
   
-  % compute gradients
-  [newGradx,newGrady]=ComputeDerivatives2(newImage);
+  % compute image center and bounds
+  xc=(M+1)/2;
+  yc=(N+1)/2;
   
-  % compute corner intensity
-  kappa=DetectCorners(newGradx,newGrady,sigma,method);
+  % construct low peak image
+  lowPeaksImage=false(M,N);
+  lowPeaksImage(lowPeaks)=true;
   
-  % find peaks
-  allPeaks=(kappa==LocalMAX(kappa,[3,3]));
+  % compute distance to nearest peak
+  R=double(bwdist(lowPeaksImage,'euclidean'));
   
-  % only select masked region
-  if(nargin>6)
-    allPeaks=allPeaks&mask;
-  end
+  % get image centered coordinates
+  [xo,yo]=ind2sub([M,N],highPeaks);
+  xo=xo-xc;
+  yo=yo-yc;
   
-  % find linear indices of low peaks
-  peakIndices=find(allPeaks);
-  [val,ind]=sort(kappa(peakIndices),'descend');
-  lowPeaks=peakIndices(ind(1:min(numPoints,numel(ind))));
-  
-  if(~isempty(highPeaks))
+  % solve for projective model
+  for k=1:numIterations
+    x=xo;
+    y=yo;
+    den=P(3,1)*x+P(3,2)*y+P(3,3);
+    xp=(P(1,1)*x+P(1,2)*y+P(1,3))./den;
+    yp=(P(2,1)*x+P(2,2)*y+P(2,3))./den;
     
-    % compute image center and bounds
-    xc=(M+1)/2;
-    yc=(N+1)/2;
-  
-    % construct low peak image
-    lowPeaksImage=false(M,N);
-    lowPeaksImage(lowPeaks)=true;
-
-    % compute distance to nearest peak
-    R=double(bwdist(lowPeaksImage,'euclidean'));
-
-    % get image centered coordinates
-    [xo,yo]=ind2sub([M,N],highPeaks);
-    xo=xo-xc;
-    yo=yo-yc;
-  
-    % solve for projective model
-    for k=1:numIterations
-      x=xo;
-      y=yo;
-      den=P(3,1)*x+P(3,2)*y+P(3,3);
-      xp=(P(1,1)*x+P(1,2)*y+P(1,3))./den;
-      yp=(P(2,1)*x+P(2,2)*y+P(2,3))./den;
-      
-      % convert to array coordinates
-      xp=xp+xc;
-      yp=yp+yc;
-
-      % stay within borders
-      good=(xp>1)&(xp<M)&(yp>1)&(yp<N);
-      xp=xp(good);
-      yp=yp(good);
-      
-      % exclude features that are badly mismatched
-      good=(R(sub2ind([M,N],floor(xp+0.5),floor(yp+0.5)))<(3*sigma));
-      xp=xp(good);
-      yp=yp(good);
-      
-      xf=floor(xp);
-      yf=floor(yp);
-      xr=xp-xf;
-      yr=yp-yf;
-
-      base=xf+M*(yf-1);
-      R00=R(base);
-      R10=R(base+1);
-      R01=R(base+M);
-      R11=R(base+(M+1));
-
-      % interpolates the derivative of 0.5R^2 by solving the following problem 
-      % 0.5*R(x,y)^2=0.5*x^2+0.5*y^2+a*x+b*y+c*x*y+d;
-      
-      R00=0.5*R00.*R00;
-      R10=0.5*R10.*R10;
-      R01=0.5*R01.*R01;
-      R11=0.5*R11.*R11;
-
-      aa=R10-R00-0.5;
-      bb=R01-R00-0.5;
-      cc=R00+R11-R10-R01;
-      % dd=R00;
-
-      SDRxi=xr+aa+cc.*yr;
-      SDRyi=yr+bb+cc.*xr;
-      
-      % minimize the SAD instead of the SSD
-      ADRxi=sqrt(abs(SDRxi));
-      ADRyi=sqrt(abs(SDRyi));
-      
-      % restore the sign
-      HSRxi=sign(SDRxi).*ADRxi;
-      HSRyi=sign(SDRyi).*ADRyi;
-
-      % convert to image centered coordinates
-      xp=xp-xc;
-      yp=yp-yc;
-      
-      xs=xp-HSRxi;
-      ys=yp-HSRyi;
-      
-      % shift to relative coordinates
-      x=xp;
-      y=yp;
-      
-      % normalize coordinates
-      x=x/N;
-      y=y/N;
-      xs=xs/N;
-      ys=ys/N;
-      
-      xx=x.*x;
-      xy=x.*y;
-      yy=y.*y;
-      xsxs=xs.*xs;
-      ysys=ys.*ys;
-      xxs=x.*xs;
-      yxs=y.*xs;
-      xys=x.*ys;
-      yys=y.*ys;
-      xxxs=xx.*xs;
-      xxys=xx.*ys;
-      xyxs=xy.*xs;
-      xyys=xy.*ys;
-      yyys=yy.*ys;
-      yyxs=yy.*xs;
-      xxsxs=x.*xsxs;
-      xysys=x.*ysys;
-      yxsxs=y.*xsxs;
-      yysys=y.*ysys;
-      xxxsxs=xx.*xsxs;
-      yyysys=yy.*ysys;
-      xxysys=xx.*ysys;
-      yyxsxs=yy.*xsxs;
-      xyxsxs=xy.*xsxs;
-      xyysys=xy.*ysys;
-      
-      A=zeros(8,8);
-      
-      A(1,1)=sum(xx);
-      A(1,2)=sum(xy);
-      A(1,3)=sum(x);
-      
-      A(2,1)=A(1,2);
-      A(2,2)=sum(yy);
-      A(2,3)=sum(y);
-      
-      A(3,1)=A(1,3);
-      A(3,2)=A(2,3);
-      A(3,3)=numel(x);
-      
-      A(4:6,4:6)=A(1:3,1:3);
-      
-      A(7,1)=-sum(xxxs);
-      A(7,2)=-sum(xyxs);
-      A(7,3)=-sum(xxs);
-      A(7,4)=-sum(xxys);
-      A(7,5)=-sum(xyys);
-      A(7,6)=-sum(xys);
-      
-      A(8,1)=A(7,2);
-      A(8,2)=-sum(yyxs);
-      A(8,3)=-sum(yxs);
-      A(8,4)=A(7,5);
-      A(8,5)=-sum(yyys);
-      A(8,6)=-sum(yys);
-      
-      A(1:6,7:8)=transpose(A(7:8,1:6));
-      
-      A(7,7)=sum(xxxsxs+xxysys);
-      A(7,8)=sum(xyxsxs+xyysys);
-      A(8,7)=A(7,8);
-      A(8,8)=sum(yyxsxs+yyysys);
-      
-      B=zeros(8,1);
-      
-      B(1)=sum(xxs);
-      B(2)=sum(yxs);
-      B(3)=sum(xs);
-      B(4)=sum(xys);
-      B(5)=sum(yys);
-      B(6)=sum(ys);
-      B(7)=-sum(xxsxs+xysys);
-      B(8)=-sum(yxsxs+yysys);
-
-      if((nargout>2)&&(k==numIterations))
-        xyCov = cov([HSRxi,HSRyi]);
-      end
-      
-      if( condest(A)>(1/eps) )      
-        X=A\B;
-
-        dP=[[X(1),X(2),X(3)]
-            [X(4),X(5),X(6)]
-            [X(7),X(8),   1]];
-
-        % undo normalization
-        dP(1:2,3)=dP(1:2,3)*N;
-        dP(3,1:2)=dP(3,1:2)/N;
-      else
-        dP = eye(3);
-      end
-      
-      % undo relative coordinates
-      P=dP*P;
-      P=P/P(9);      
+    % convert to array coordinates
+    xp=xp+xc;
+    yp=yp+yc;
+    
+    % stay within borders
+    good=(xp>1)&(xp<M)&(yp>1)&(yp<N);
+    xp=xp(good);
+    yp=yp(good);
+    
+    % exclude features that are badly mismatched
+    good=(R(sub2ind([M,N],floor(xp+0.5),floor(yp+0.5)))<(3*sigma));
+    xp=xp(good);
+    yp=yp(good);
+    
+    xf=floor(xp);
+    yf=floor(yp);
+    xr=xp-xf;
+    yr=yp-yf;
+    
+    base=xf+M*(yf-1);
+    R00=R(base);
+    R10=R(base+1);
+    R01=R(base+M);
+    R11=R(base+(M+1));
+    
+    % interpolates the derivative of 0.5R^2 by solving the following problem
+    % 0.5*R(x,y)^2=0.5*x^2+0.5*y^2+a*x+b*y+c*x*y+d;
+    
+    R00=0.5*R00.*R00;
+    R10=0.5*R10.*R10;
+    R01=0.5*R01.*R01;
+    R11=0.5*R11.*R11;
+    
+    aa=R10-R00-0.5;
+    bb=R01-R00-0.5;
+    cc=R00+R11-R10-R01;
+    % dd=R00;
+    
+    SDRxi=xr+aa+cc.*yr;
+    SDRyi=yr+bb+cc.*xr;
+    
+    % minimize the SAD instead of the SSD
+    ADRxi=sqrt(abs(SDRxi));
+    ADRyi=sqrt(abs(SDRyi));
+    
+    % restore the sign
+    HSRxi=sign(SDRxi).*ADRxi;
+    HSRyi=sign(SDRyi).*ADRyi;
+    
+    % convert to image centered coordinates
+    xp=xp-xc;
+    yp=yp-yc;
+    
+    xs=xp-HSRxi;
+    ys=yp-HSRyi;
+    
+    % shift to relative coordinates
+    x=xp;
+    y=yp;
+    
+    % normalize coordinates
+    x=x/N;
+    y=y/N;
+    xs=xs/N;
+    ys=ys/N;
+    
+    xx=x.*x;
+    xy=x.*y;
+    yy=y.*y;
+    xsxs=xs.*xs;
+    ysys=ys.*ys;
+    xxs=x.*xs;
+    yxs=y.*xs;
+    xys=x.*ys;
+    yys=y.*ys;
+    xxxs=xx.*xs;
+    xxys=xx.*ys;
+    xyxs=xy.*xs;
+    xyys=xy.*ys;
+    yyys=yy.*ys;
+    yyxs=yy.*xs;
+    xxsxs=x.*xsxs;
+    xysys=x.*ysys;
+    yxsxs=y.*xsxs;
+    yysys=y.*ysys;
+    xxxsxs=xx.*xsxs;
+    yyysys=yy.*ysys;
+    xxysys=xx.*ysys;
+    yyxsxs=yy.*xsxs;
+    xyxsxs=xy.*xsxs;
+    xyysys=xy.*ysys;
+    
+    A=zeros(8,8);
+    
+    A(1,1)=sum(xx);
+    A(1,2)=sum(xy);
+    A(1,3)=sum(x);
+    
+    A(2,1)=A(1,2);
+    A(2,2)=sum(yy);
+    A(2,3)=sum(y);
+    
+    A(3,1)=A(1,3);
+    A(3,2)=A(2,3);
+    A(3,3)=numel(x);
+    
+    A(4:6,4:6)=A(1:3,1:3);
+    
+    A(7,1)=-sum(xxxs);
+    A(7,2)=-sum(xyxs);
+    A(7,3)=-sum(xxs);
+    A(7,4)=-sum(xxys);
+    A(7,5)=-sum(xyys);
+    A(7,6)=-sum(xys);
+    
+    A(8,1)=A(7,2);
+    A(8,2)=-sum(yyxs);
+    A(8,3)=-sum(yxs);
+    A(8,4)=A(7,5);
+    A(8,5)=-sum(yyys);
+    A(8,6)=-sum(yys);
+    
+    A(1:6,7:8)=transpose(A(7:8,1:6));
+    
+    A(7,7)=sum(xxxsxs+xxysys);
+    A(7,8)=sum(xyxsxs+xyysys);
+    A(8,7)=A(7,8);
+    A(8,8)=sum(yyxsxs+yyysys);
+    
+    B=zeros(8,1);
+    
+    B(1)=sum(xxs);
+    B(2)=sum(yxs);
+    B(3)=sum(xs);
+    B(4)=sum(xys);
+    B(5)=sum(yys);
+    B(6)=sum(ys);
+    B(7)=-sum(xxsxs+xysys);
+    B(8)=-sum(yxsxs+yysys);
+    
+    if((nargout>2)&&(k==numIterations))
+      xyCov = cov([HSRxi,HSRyi]);
     end
+    
+    if(condest(A)<(1.0/(1000.0*eps)))
+      X=A\B;
+      
+      dP=[[X(1),X(2),X(3)]
+        [X(4),X(5),X(6)]
+        [X(7),X(8),   1]];
+      
+      % undo normalization
+      dP(1:2,3)=dP(1:2,3)*N;
+      dP(3,1:2)=dP(3,1:2)/N;
+    else
+      dP = eye(3);
+    end
+    
+    % undo relative coordinates
+    P=dP*P;
+    P=P/P(9);
   end
+end
 
+end
+
+% Computes the gradients of one or two arrays
+% Uses a 4-element finite difference approximation
+% Optionally computes the corresponding difference between arrays
+% Removes one-pixel borders that do not make sense by convolution
+%
+%    y = array (m-by-n)
+% yref = reference array (m-by-n)
+%   fi = row gradient (m-by-n)
+%   fj = column gradient (m-by-n)
+%   ft = corresponding array differences (m-by-n)
+function [fi,fj,ft]=ComputeDerivatives2(y,yref)
+
+if (size(y,3)~=1)
+  error('input must be a 2-dimensional array');
+end
+
+if nargin==2
+  if (size(y,1) ~= size(yref,1)) || (size(y,2) ~= size(yref,2))
+    error('input arrays must be of equal size');
+  end
+  if (size(yref,3)~=1)
+    error('input must be a 2-dimensional array');
+  end
+end
+
+imask=[
+  0  0  0;
+  0 -1 -1;
+  0  1  1]/2;
+
+jmask=[
+  0  0  0;
+  0 -1  1;
+  0 -1  1]/2;
+
+fi=filter2(imask,y);
+fj=filter2(jmask,y);
+
+fi=RemoveBorders(fi,1);
+fj=RemoveBorders(fj,1);
+
+if nargin==2
+  tmask=[
+    0  0  0;
+    0  1  1;
+    0  1  1]/4;
+  ft=filter2(tmask,y)+filter2(-tmask,yref);
+  ft=RemoveBorders(ft,1);
+else
+  ft=[];
+end
+
+end
+
+%Removes or replaces borders of an image
+%
+% w = width of border
+% v = value to put in border region (default=0)
+function y = RemoveBorders(y,w,v)
+
+if nargin<3
+  v=0;
+end
+
+y(:,1:w)=v;
+y(:,end-(0:(w-1)))=v;
+y(1:w,:)=v;
+y(end-(0:(w-1)),:)=v;
+
+end
+
+function kappa=DetectCorners(gi,gj,halfwin,method)
+
+win=(2*halfwin+1)*[1,1];
+
+%formulate the gradient products
+gxx=gi.*gi;
+gyy=gj.*gj;
+gxy=gi.*gj;
+
+%Perform smoothing or local sum
+xx=Smooth2(gxx,win,halfwin/4);
+yy=Smooth2(gyy,win,halfwin/4);
+xy=Smooth2(gxy,win,halfwin/4);
+
+%calculate corner intensity
+kappa=feval(method,xx,yy,xy);
+
+end
+
+% Performs gaussian smoothing over a window
+%
+% I=intensity image
+% sig=smoothing factor
+% OUT=output image
+function OUT=Smooth2(I,win,sig)
+M=fspecial('gaussian',win,sig);
+OUT=filter2(M,I);
+end
+
+% The corner detector of Harris and Stephens
+% based on the symmetric 2x2 matrix
+% [xx xy]
+% [xy yy]
+%
+% Where xx,yy,xy are sums of local image gradients
+function val=HarrisCorner(xx,yy,xy)
+val=(xx.*yy-xy.*xy)./(xx+yy+eps);
+end
+
+% LocalMAX() calculates the local maximum using a sliding window
+%
+% win(1) is the height of the window
+% win(2) is the width of the window
+function I=LocalMAX(I,win)
+I=colfilt(I,win,'sliding','max');
 end
