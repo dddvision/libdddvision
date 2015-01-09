411fb4ca7b628dacdc64be6b204421c75fe6e6d8
dddvision <dddvision@5c6639e4-15fb-11df-801b-2d21f7a92e13>
2015-01-09 15:24:17 +0000

    Incremental development update to denseCorrespondence

--- trunk/denseCorrespondence.m
+++ trunk/denseCorrespondence.m
@@ -1,9 +1,21 @@
-function [di, dj] = denseCorrespondence(a, b)
+% Compute dense correspondence field given two images and constraints.
+%
+% @param[in] a     first image
+% @param[in] b     second image
+% @param[in] diMax maximum absolute value of displacement in step direction
+% @param[in] djMax maximum absolute value of displacement in stride direction
+% @param[in] dtMax maximum absolute value of displacement in angle
+% @param[in] dsMax maximum absolute value of displacement in scale
+%
+%IDEAS:
+% use tiny 3x3 or 2x2 support
+% allow small rotation and scale
+function [di, dj] = denseCorrespondence(a, b, diMax, djMax, dtMax, dsMax)
 if(nargin==0)
   close('all');
   a = imread('data/RubberWhaleA.png');
   b = imread('data/RubberWhaleB.png');
-  [di, dj] = denseCorrespondence(a, b);
+  [di, dj] = denseCorrespondence(a, b, diMax, djMax, dtMax, dsMax);
   rgb = colorize(di, dj, 5);
   figure;
   imshow([a, b, 255*rgb]);
@@ -14,6 +26,10 @@ end
 %load('data/RubberWhaleFlow.mat');
 %di = diTruth;
 %dj = djTruth;
+end
+
+function d = matchMetric(a, b, pi, pj)
+
 end
 
 function rgb = colorize(di, dj, rMax)
