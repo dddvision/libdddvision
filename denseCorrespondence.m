d55be055981e3b9f32c68fa68ef7057638e31a98
dddvision <dddvision@5c6639e4-15fb-11df-801b-2d21f7a92e13>
2015-01-10 01:05:44 +0000

    More updates to denseCorrespondence

--- denseCorrespondence.m
+++ denseCorrespondence.m
@@ -1,11 +1,12 @@
 % Compute dense correspondence field given two images and constraints.
 %
-% @param[in] a     first image
-% @param[in] b     second image
-% @param[in] diMax maximum absolute value of displacement in step direction
-% @param[in] djMax maximum absolute value of displacement in stride direction
-% @param[in] dtMax maximum absolute value of displacement in angle
-% @param[in] dsMax maximum absolute value of displacement in scale
+% @param[in]  a          first image
+% @param[in]  b          second image
+% @param[in]  dStrideMax maximum absolute value of displacement in stride direction (pixels)
+% @param[in]  dStepMax   maximum absolute value of displacement in step direction (pixels)
+% @param[in]  dAngleMax  maximum absolute value of displacement in angle CCW from right (radians)
+% @param[out] dStride    displacement in stride direction (pixels)
+% @param[out] dStep      displacement in step direction (pixels)
 %
 %IDEAS:
 % use tiny 3x3 support
@@ -16,32 +17,65 @@
 % use concept of propigation
 % pre-compute (cubic?) interpolation factors over image b so it can be queried quickly
 % invent descriptor that finds the "corner postion and orientation" given a 3x3
-% derive di, dj, dt, ds from variational theory
-function [di, dj] = denseCorrespondence(a, b, diMax, djMax, dtMax, dsMax)
+% derive di, dj, dt from variational theory
+function [dStride, dStep] = denseCorrespondence(a, b, dStrideMax, dStepMax, dAngleMax)
 if(nargin==0)
   close('all');
   a = imread('data/RubberWhaleA.png');
   b = imread('data/RubberWhaleB.png');
-  [di, dj] = denseCorrespondence(a, b, diMax, djMax, dtMax, dsMax);
-  rgb = colorize(di, dj, 5);
+  dStrideMax = 0.5;
+  dStepMax = 0.5;
+  dAngleMax = atan(0.5);
+  [dStride, dStep] = denseCorrespondence(a, b, dStrideMax, dStepMax, dAngleMax);
+  rgb = colorize(dStride, dStep, 5.0);
   figure;
   imshow([a, b, 255*rgb]);
-  di = [];
-  dj = [];
+  dStride = [];
+  dStep = [];
   return;
 end
-%load('data/RubberWhaleFlow.mat');
-%di = diTruth;
-%dj = djTruth;
+step = 5;
+stride = 5;
+dStep = 0.25;
+dStride = 0.25;
+dAngle = 0.01;
+d = matchMetric(a, b, step, stride, dStep, dStride, dAngle);
+load('data/RubberWhaleFlow.mat');
+dStride = dStrideTruth;
+dStep = dStepTruth;
 end
 
-function d = matchMetric(a, b, pi, pj)
-
+function d = matchMetric(a, b, stride, step, dStride, dStep, dAngle)
+persistent M N sStride sStep
+if(isempty(M))
+  [M, N] = size(a);
+  sStride = [-1, 0, 1; -1, 0, 1; -1, 0, 1];
+  sStep = [-1, -1, -1; 0, 0, 0; 1, 1, 1];
+end
+a = double(a(:, :, 1)); % TODO: use all three colors
+b = double(b(:, :, 1)); % TODO: use all three colors
+ns = sStride;
+ms = sStep;
+as = a(sub2ind([M, N], step+ms(:), stride+ns(:)));
+[ns, ms] = transform(sStride, sStep, dStride, dStep, dAngle);
+bs = interpolate(b, step+ms(:), stride+ns(:));
+d = 0.0;
 end
 
-function rgb = colorize(di, dj, rMax)
-r = sqrt(di.*di+dj.*dj);
+function ys = interpolate(y, ms, ns)
+ys = interp2(y, ns+1, ms+1);
+end
+
+function [xt, yt] = transform(x, y, dx, dy, da)
+c = cos(da);
+s = sin(da);
+xt = x.*c-y.*s+dx;
+yt = x.*s+y.*c+dy;
+end
+
+function rgb = colorize(dStride, dStep, rMax)
+r = sqrt(dStep.*dStep+dStride.*dStride);
 r(r>1e9) = nan;
-theta = atan2(di, -dj);
+theta = atan2(dStep, -dStride);
 rgb = hsv2rgb(0.5+theta./(2.0*pi), r./rMax, ones(size(r)));
 end
