ca34de39df24ed424b735c6948169c11b95989ed
David Diel <dddvision@gmail.com>
2016-08-18 18:00:04 -0400

    Adding examples of MATLAB classes

---
+++ Counter.m
@@ -0,0 +1,24 @@
+% Example of a trivial class.
+%
+% >> c = Counter();
+% >> c.count
+% ans = 1
+% >> c.count
+% ans = 2
+% >> c.count
+% ans = 3
+classdef Counter < handle
+  properties
+    x
+  end
+  
+  methods
+    function y = count(this)
+      if(isempty(this.x))
+        this.x = 0;
+      end
+      this.x = this.x+1;
+      y = this.x;      
+    end
+  end
+end
---
+++ MyClass.m
@@ -0,0 +1,39 @@
+% Example of a class with a few methods.
+%
+% Usage:
+% >> c = MyClass(1);
+% >> c.getState()
+% ans = 1
+% >> c.setState(2)
+% >> c.getState()
+% ans = 2
+% >> c.operate(3)
+% ans = 5
+% >> c.delete()
+classdef MyClass < handle
+  properties (GetAccess = private, SetAccess = private)
+    state
+  end
+  
+  methods (Access = public)
+    function this = MyClass(x0)
+      this.state = x0;
+    end
+    
+    function setState(this, x)
+      this.state = x;
+    end
+    
+    function x = getState(this)
+      x = this.state;
+    end
+    
+    function z = operate(this, y)
+      z = this.state+y;
+    end
+
+    function delete(this)
+      this.state = [];
+    end
+  end
+end
